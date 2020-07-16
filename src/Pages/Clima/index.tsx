d9e86c8bd0c8882be3731044f667ba2aec94522b
Vinicius Lima <paulo.vinicius.f.l@live.com>
2020-07-16 13:50:11 -0300

    ocorrendo um erro na requisição tem que verificar a questão

--- src/Pages/Clima/index.tsx
+++ src/Pages/Clima/index.tsx
@@ -10,7 +10,7 @@
 
 🙌 DIFERENCIAIS
 
-Será muito bem valorizado:
+Será muito bem valorizado: 
 
     Arquitetura
     Documentação
@@ -21,107 +21,99 @@ import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, Text, PermissionsAndroid } from 'react-native';
 import Geolocation2 from '@react-native-community/geolocation';
 
-// Geolocation2.setRNConfiguration();
-//Geolocalização2.setRNConfiguration ( config ) ;
 
 import Geolocation from 'react-native-geolocation-service';
 import api from '../../services/api';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'lightgreen',
-  },
-  text: {
-    color: 'orange',
-    fontSize: 40,
-  },
-});
+import { Container } from './style';
 
 const apikey = 'd55143bf18a6deb7e90c8f3a26783805';
 
 interface Coordenadas {
-  latitude: number,
-  longitude: number,
+    latitude: number,
+    longitude: number,
 }
 
+
+
 export default function Clima() {
-  const [hasLocationPermission, setHasLocationPermission] = useState(false);
-  const [userPosition, setUserPosition] = useState<Coordenadas>(Object);
-  const [coords, setCoords] = useState({});
-  const [weather, setWeather] = useState({});
+    const [hasLocationPermission, setHasLocationPermission] = useState(false);
+    const [userPosition, setUserPosition] = useState<Coordenadas>(Object);
+    const [coords, setCoords] = useState({});
+    const [weather, setWeather] = useState(Object);
 
 
-  //Geolocation2.getCurrentPosition((info) => console.log(info));
-
-  async function verifyLocationPermission() {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log('permissão concedida');
-        setHasLocationPermission(true);
-      } else {
-        console.error('permissão negada');
-        setHasLocationPermission(false);
-      }
-    } catch (err) {
-      console.warn(err);
+    async function verifyLocationPermission() {
+        try {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            );
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                console.log('permissão concedida');
+                setHasLocationPermission(true);
+            } else {
+                console.error('permissão negada');
+                setHasLocationPermission(false);
+            }
+        } catch (err) {
+            console.warn(err);
+        }
     }
-  }
 
-  useEffect(() => {
-    verifyLocationPermission();
-    async function handleLocation() {
-      if (hasLocationPermission) {
-        await Geolocation.getCurrentPosition(
-          position => {
-            setUserPosition({
-              latitude: position.coords.latitude,
-              longitude: position.coords.longitude,
-            });
-          },
-          error => {
-            console.log(error.code, error.message);
-          }
-        );
-      }
-    }
-    handleLocation();
+    useEffect(() => {
+        verifyLocationPermission();
+        async function handleLocation() {
+            if (hasLocationPermission) {
+                await Geolocation.getCurrentPosition(
+                    position => {
+                        setUserPosition({
+                            latitude: position.coords.latitude,
+                            longitude: position.coords.longitude,
+                        });
+                    },
+                    error => {
+                        console.log(error.code, error.message);
+                    }
+                );
+            }
+        }
+        handleLocation();
 
-  }, [hasLocationPermission]);
-
+    }, [hasLocationPermission]);
 
 
-  const { latitude, longitude } = userPosition;
 
-  const caminho = `2.5/weather?lat=${userPosition.latitude}&lon=${userPosition.longitude}&appid=${apikey}`
- 
+    const { latitude, longitude } = userPosition;
 
-  useEffect(() => {
-    async function handleClima(): Promise<void> {
-      try {
-        await api.get(caminho).then((response) => {
-          setWeather(response.data);
-          console.log(response.data);
-
-        });
-      } catch (err) {
-        console.error(err,);
-
-      }
-
-    }
-    handleClima();
-
-  }, []);
+    const caminho = `2.5/weather?lat=${userPosition.latitude}&lon=${userPosition.longitude}&appid=${apikey}`
 
 
-  return (
-    <View style={styles.container}>
-      <Text>Latitude: {latitude}</Text>
-      <Text>Longitude: {longitude}</Text>
-    </View>
-  );
+    useEffect(() => {
+        async function handleClima(): Promise<void> {
+            try {
+                await api.get(caminho).then((response) => {
+                    setWeather(response.data);
+                    console.log(response.data)
+
+                });
+            } catch (err) {
+                console.error(err,);
+
+            }
+
+        }
+        handleClima();
+
+    }, []);
+
+
+    return (
+        <Container >
+            <Text>Latitude: {latitude}</Text>
+            <Text>Longitude: {longitude}</Text>
+            <Text>{weather.name}</Text>
+            <Text>Temperatura {weather.main.temp  - 273.15}</Text>
+            <Text>Temp.Max:{weather.main.temp_max - 273.15}</Text>
+            <Text>Temp.Min:{weather.main.temp_min - 273.15}</Text>
+        </Container>
+    );
 }
--- src/Pages/Clima/style.ts
+++ src/Pages/Clima/style.ts
@@ -1,5 +1,8 @@
-import styled from "styled-components";
+import styled from "styled-components/native";
 
-export const Container = styled.view`
+export const Container = styled.View`
+flex:1;
+background:#3399ff;
 
 `;
+

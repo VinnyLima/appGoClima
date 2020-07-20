1ad66d29a2bac27e9296dd61d21f3ef0cc5f428c
Vinicius Lima <paulo.vinicius.f.l@live.com>
2020-07-20 01:30:53 -0300

    realizando tratativas de response for object

--- src/Pages/Clima/index.tsx
+++ src/Pages/Clima/index.tsx
@@ -18,124 +18,157 @@ Será muito bem valorizado:
 
  */
 
-import React, { useCallback, useState, useEffect } from 'react';
-import { View, StyleSheet, Text, PermissionsAndroid } from 'react-native';
+import React, {useCallback, useState, useEffect} from 'react';
+import {PermissionsAndroid, View, Text} from 'react-native';
 import moment from 'moment';
+import 'moment/locale/pt-br';
 import Icon from 'react-native-vector-icons/Feather';
-import { Container, Reload, Cidade, Bairro, DadosEnd, Day, TempWeather, Temp, MinMaxTemp, Min, Max } from './style';
+import {
+  Container,
+  Reload,
+  Cidade,
+  Bairro,
+  DadosEnd,
+  Day,
+  TempWeather,
+  Temp,
+  MinMaxTemp,
+  Min,
+  Max,
+} from './style';
 import Geolocation from 'react-native-geolocation-service';
 import api from '../../services/api';
-
-
+import RNRestart from 'react-native-restart';
 
 const apikey = 'd55143bf18a6deb7e90c8f3a26783805';
 
 interface Coordenadas {
-    latitude: number;
-    longitude: number;
+  latitude: number;
+  longitude: number;
 }
 
-// interface WeatherObject {
-//     description: string;
-//     icon: string;
-//     id: number;
-//     main: string;
-//   }
-  
-//   interface Response {
-//       name: string
-//     main: {
-//       feels_like: number;
-//       humidity: number;
-//       pressure: number;
-//       temp: number;
-//       temp_max: number;
-//       temp_min: number;
-//     };
-//     weather: WeatherObject[];
-//   }
+interface Response {
+  name: string;
+  main: {
+    feels_like: number;
+    humidity: number;
+    pressure: number;
+    temp: number;
+    temp_max: number;
+    temp_min: number;
+  };
+}
 
 //   console.log(weather.main.temp);
 export default function Clima() {
-    const [hasLocationPermission, setHasLocationPermission] = useState(false);
-    const [userPosition, setUserPosition] = useState<Coordenadas>(Object);
-    const [coords, setCoords] = useState({});
-    const [weather, setWeather] = useState<Response>({}as Response);
+  const [hasLocationPermission, setHasLocationPermission] = useState(false);
+  const [weather, setWeather] = useState<Response>(Object);
+  const [userPosition, setUserPosition] = useState<Coordenadas>(Object);
+  const [verifiObject, setVerifiObject] = useState(false);
 
-    const today = moment().format('dddd');
+  moment.locale('pt-br');
+  const today = moment().calendar();
 
+  async function verifyLocationPermission() {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('permissão concedida');
+        setHasLocationPermission(true);
+      } else {
+        console.error('permissão negada');
+        setHasLocationPermission(false);
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+  useEffect(() => {
+    verifyLocationPermission();
 
-
-    async function verifyLocationPermission() {
-        try {
-            const granted = await PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-            );
-            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                console.log('permissão concedida');
-                setHasLocationPermission(true);
-            } else {
-                console.error('permissão negada');
-                setHasLocationPermission(false);
-            }
-        } catch (err) {
-            console.warn(err);
-        }
+    async function handleLocation() {
+      if (hasLocationPermission) {
+        await Geolocation.getCurrentPosition(
+          (position) => {
+            console.log(position);
+            setUserPosition({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            });
+          },
+          (error) => {
+            // See error code charts below.
+            console.log(error.code, error.message);
+          },
+          {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+        );
+      }
     }
 
-    useEffect(() => {
-        verifyLocationPermission();
-        async function handleLocation() {
-            if (hasLocationPermission) {
-                await Geolocation.getCurrentPosition(
-                    (position) => {
-                        setUserPosition({
-                            latitude: position.coords.latitude,
-                            longitude: position.coords.longitude,
-                        });
-                    },
-                    (error) => {
-                        console.log(error.code, error.message);
-                    },
-                );
-                if (userPosition.latitude != null) {
-                    await api.get(`2.5/weather?lat=${userPosition.latitude}&lon=${userPosition.longitude}&appid=${apikey}`).then((response) => {
-                        setWeather(response.data);
+    handleLocation();
+  }, [hasLocationPermission]);
 
-                    })
-                }
-
-
-            }
+  useEffect(() => {
+    try {
+      if (userPosition.latitude && userPosition.longitude) {
+        async function handleWealther(latitude, longitude) {
+          var response = await api.get(
+            `2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`,
+          );
+          if (response.data) {
+            setVerifiObject(true);
+            setWeather(response.data);
+          }
         }
-        handleLocation();
-    }, [hasLocationPermission]);
+        handleWealther(userPosition.latitude, userPosition.longitude);
+      }
+    } catch (error) {
+      console.log(error.code, error.message);
+    }
+  }, [userPosition.latitude, userPosition.longitude]);
 
-    console.log(userPosition.latitude, userPosition.longitude);
-    console.log(weather);
+  console.log(verifiObject);
 
-
+  if (verifiObject === false) {
     return (
-        <Container>
-            <Reload onPress={() => {
-                console.log('reload')
-
-            }}>
-                <Icon name="refresh-cw" size={30} color="#404040" />
-            </Reload>
-            <DadosEnd>
-                <Cidade>{weather.name}</Cidade>
-                <Bairro>Porto Novo</Bairro>
-                <Day>{today}</Day>
-            </DadosEnd>
-            <TempWeather>
-                <Icon name="sun" size={60} color="#404040" />
-                <Temp>{(weather.main.temp - 273.15).toFixed(0)}°</Temp>
-            </TempWeather>
-            <MinMaxTemp>
-                <Max>{(weather.main.temp_max - 273.15).toFixed(0)}°<Icon name="sunrise" size={20} color="#404040" /></Max>
-                <Min>{(weather.main.temp_min - 273.15).toFixed(0)}º<Icon name="sunset" size={20} color="#404040" /></Min>
-            </MinMaxTemp>
-        </Container>
+      <Container>
+        <View>
+          <Text>Carregando</Text>
+        </View>
+      </Container>
     );
+  } else {
+    return (
+      <Container>
+        <Reload
+          onPress={() => {
+            RNRestart.Restart();
+          }}>
+          <Icon name="refresh-cw" size={30} color="#404040" />
+        </Reload>
+        <DadosEnd>
+          <Cidade>{weather.name}</Cidade>
+          <Bairro>Porto Novo</Bairro>
+          <Day>{today}</Day>
+        </DadosEnd>
+        <TempWeather>
+          <Icon name="sun" size={60} color="#404040" />
+          <Temp>{(weather.main.temp - 273.15).toFixed(0)}°</Temp>
+        </TempWeather>
+        <MinMaxTemp>
+          <Max>
+            {(weather.main.temp_max - 273.15).toFixed(0)}°
+            <Icon name="sunrise" size={20} color="#404040" />
+          </Max>
+
+          <Min>
+            {(weather.main.temp_min - 273.15).toFixed(0)}°
+            <Icon name="sunset" size={20} color="#404040" />
+          </Min>
+        </MinMaxTemp>
+      </Container>
+    );
+  }
 }
--- src/Pages/Clima/style.ts
+++ src/Pages/Clima/style.ts
@@ -6,75 +6,61 @@ export const Container = styled.SafeAreaView`
 `;
 
 export const Reload = styled.TouchableOpacity`
-  position:absolute;
-  right:25px; 
+  position: absolute;
+  right: 25px;
   top: 15px;
-
 `;
 
 export const DadosEnd = styled.View`
-position:relative;
-width:150px;
-top: 40px;
-left: 30px;
-
+  position: relative;
+  width: 150px;
+  top: 40px;
+  left: 30px;
 `;
 export const Cidade = styled.Text`
-
-  color:#404040;
-  font-size:35px;
+  color: #404040;
+  font-size: 35px;
   font-weight: 500;
-  
 `;
 
 export const Bairro = styled.Text`
-  color:#404040;
-  font-size:18px; 
-  
+  color: #404040;
+  font-size: 18px;
 `;
 
 export const Day = styled.Text`
-color:#404040;
-  font-size:18px;
+  color: #404040;
+  font-size: 18px;
   margin-top: 30px;
   font-weight: 500;
-
 `;
 
 export const TempWeather = styled.View`
-align-content:center;
-align-items:center;
-margin-top: 125px;
-
+  align-content: center;
+  align-items: center;
+  margin-top: 125px;
 `;
 
 export const Temp = styled.Text`
-  font-size:80px;
-  color:#404040;
-
+  font-size: 80px;
+  color: #404040;
 `;
 
-
 export const MinMaxTemp = styled.View`
-align-content:space-between;
-justify-content:center;
-align-items:center;
-flex-direction:row;
-margin-top: 95px;
+  align-content: space-between;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  margin-top: 95px;
 `;
 
 export const Min = styled.Text`
-  font-size:30px;
-  color:#404040;
-  margin-left:30px;
- 
+  font-size: 30px;
+  color: #404040;
+  margin-left: 30px;
 `;
 export const Max = styled.Text`
-  font-size:30px;
-  color:#404040;
-  margin-right:30px;
-
-
-
+  font-size: 30px;
+  color: #404040;
+  margin-right: 30px;
 `;
-
